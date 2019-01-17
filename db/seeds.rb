a6885640dd1d2e070b1ce5d3b0b0947af260ecc6
pnb90 <phuocbui90@gmail.com>
2019-01-16 22:35:48 -0600

    w2d3 - seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+
+  contact = Contact.new(
+                        first_name: first_name,
+                        last_name: last_name,
+                        email: Faker::Internet.free_email("#{first_name}.#{last_name}"),
+                        phone_number: Faker::PhoneNumber.phone_number
+                        )
+
+  contact.save
+end
