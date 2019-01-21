497fd294cfbaa36c4f385b73a93710a112ce69d0
pnb90 <phuocbui90@gmail.com>
2019-01-21 13:31:28 -0600

    updated index/view /w partials

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
 class Contact < ApplicationRecord
+  def friendly_updated_at
+    updated_at.strftime("%A, %d %b %Y %l:%M %p")
+  end
+
+  def full_name
+    "#{ first_name } #{ last_name }"
+  end
+
+  def japanese_numbers
+    "+81 #{ phone_number }"
+  end
 end
