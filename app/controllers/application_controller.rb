c1427b0e6fa18715ecea4601f8854e6375e85f0a
pnb90 <phuocbui90@gmail.com>
2019-01-30 19:47:38 -0600

    updated controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,17 +7,16 @@ class ApplicationController < ActionController::Base
       token = auth_headers[/(?<=\A(Bearer ))\S+\z/]
       begin
         decoded_token = JWT.decode(
-        token,
-        Rails.application.credentials.fetch(:secret_key_base),
-        true,
-        { algorithm: 'HS256' }
-       )
-       User.find_by(id: decoded_token[0]["user_id"])
+                                  token,
+                                  Rails.application.credentials.fetch(:secret_key_base),
+                                  true,
+                                  { algorithm: 'HS256' }
+                                  )
+        User.find_by(id: decoded_token[0]["user_id"])
       rescue JWT::ExpiredSignature
         nil
       end
     end
   end
-
   helper_method :current_user
 end
